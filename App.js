610383c3d55d6429f32cb9f43de02b195fa84425
DESKTOP-GMUA8LU\mckeen learning <asmamckeen@gmail.com>
2020-11-23 11:20:04 +0800

    added backend

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 // @refresh reset
 
+// States
 import React, { useState, useEffect, useCallback } from 'react'
+// Chat API
 import { GiftedChat } from 'react-native-gifted-chat'
+// Local Storage
 import AsyncStorage from '@react-native-community/async-storage'
+// Component Imports
 import { StyleSheet, TextInput, View, YellowBox, Button } from 'react-native'
+// Firebase
 import * as firebase from 'firebase'
 import 'firebase/firestore'
 // expo install react-native-gifted-chat @react-native-community/async-storage firebase
+
+// config from firebase
 const firebaseConfig = {
     apiKey: "AIzaSyCyZNdoMTX3QbLVPv61ghTGOySrZFT_-IQ",
     authDomain: "react-native-chat-clone.firebaseapp.com",
@@ -18,21 +25,34 @@ const firebaseConfig = {
     measurementId: "G-GXMJ24FSFC"
 };
 
+// This is to avoid error
 if (firebase.apps.length === 0) {
     firebase.initializeApp(firebaseConfig)
     // firebase.analytics();
 }
 
+// This is a bug na samok karon sa react native android
 YellowBox.ignoreWarnings(['Setting a timer for a long period of time'])
 
+
+// accessing firebase database
 const db = firebase.firestore()
+// creating db collection or table
 const chatsRef = db.collection('chats')
 
+// mao nani ang para mudagan ang app
 export default function App() {
+
+    // Initialization area, dira na mag gamit ug use state
     const [user, setUser] = useState(null)
     const [name, setName] = useState('')
     const [messages, setMessages] = useState([])
+    // Ang use state kay first naa siyay variable then ang sunod kay function
+    // para ma change ang value sa variable
 
+    // kani tong sa database na bali diri tong gina kuha ang mga data sa firebase
+    // ang purpose By using this Hook, you tell React that your component needs 
+    // to do something after render
     useEffect(() => {
         readUser()
         const unsubscribe = chatsRef.onSnapshot((querySnapshot) => {
@@ -51,12 +71,23 @@ export default function App() {
         return () => unsubscribe()
     }, [])
 
+    // Kani tong hook na ang purpose of this callback function is to change a piece of the state 
+    // that is a part of the parent component
     const appendMessages = useCallback(
         (messages) => {
+            // as we can see gina append niya ang component na giftedchat sa both prev and pres messages
             setMessages((previousMessages) => GiftedChat.append(previousMessages, messages))
         },
+        // tapos diri need na ihatag ang array/state
         [messages]
     )
+
+    // Async functions always return a promise
+    // Ang promise kay kanang 
+    // A Promise object is simply a wrapper around a value that may or may not 
+    // be known when the object is instantiated and provides a method for handling 
+    // the value after it is known (also known as resolved ) or 
+    // is unavailable for a failure reason (we'll refer to this as rejected ).
 
     async function readUser() {
         const user = await AsyncStorage.getItem('user')
@@ -70,11 +101,14 @@ export default function App() {
         await AsyncStorage.setItem('user', JSON.stringify(user))
         setUser(user)
     }
+    // diri kay ge kuha na niya ang mga data na naa didto sa promise or values
     async function handleSend(messages) {
         const writes = messages.map((m) => chatsRef.add(m))
         await Promise.all(writes)
     }
 
+    // diria tan awon sa program if wala bay user
+    // if wala then mag ask siya for name
     if (!user) {
         return (
             <View style={styles.container}>
@@ -83,9 +117,11 @@ export default function App() {
             </View>
         )
     }
+    // if naa mag return siya atong mga past messages
     return <GiftedChat messages={messages} user={user} onSend={handleSend} />
 }
 
+// tapos diri styles nani diri
 const styles = StyleSheet.create({
     container: {
         flex: 1,
